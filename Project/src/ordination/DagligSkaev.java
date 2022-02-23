77419bd4c9bc9fa72dd080f9161da99b285a8e41
ellog <ellogovnor47@gmail.com>
2022-02-23 12:09:14 +0100

    DagligSkaev

--- Project/src/ordination/DagligSkaev.java
+++ Project/src/ordination/DagligSkaev.java
@@ -1,12 +1,49 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
+public class DagligSkaev extends Ordination{
+    private LocalTime[] tider;
+    private double[] antalende;
+    ArrayList<Dosis> doses=new ArrayList<>();
+    public DagligSkaev(LocalDate startDen, LocalDate slutDen, Patient patient, Laegemiddel laegemiddel,LocalTime[] tid,double[] antal) {
+        super(startDen, slutDen, patient, laegemiddel);
+        this.antalende=antal;
+        this.tider=tid;
+        for (int i=0;i<tid.length;i++){
+            opretDosis(tid[i],antal[i]);
+        }
+    }
     // TODO
 
 
     public void opretDosis(LocalTime tid, double antal) {
-        // TODO
+        Dosis d=new Dosis(tid, antal);
+            doses.add(d);
+    }
+
+    @Override
+    public double samletDosis() {
+        double sum=0;
+        for (Dosis dose : doses) {
+            sum += dose.getAntal();
+        }
+        return sum;
+    }
+
+    @Override
+    public double doegnDosis() {
+        double sum=0;
+        for (Dosis dose : doses) {
+            sum += dose.getAntal();
+        }
+        return sum/antalDage();
+    }
+
+    @Override
+    public String getType() {
+        return getLaegemiddel().getNavn();
     }
 }
