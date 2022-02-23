76673bfc9138aa7c9ef11918baf7dbb65baae50a
Mathias <tidspe@gmail.com>
2022-02-23 10:35:41 +0100

    PN Commited

--- Project/src/ordination/Ordination.java
+++ Project/src/ordination/Ordination.java
@@ -8,8 +8,14 @@ public abstract class Ordination {
     private LocalDate slutDen;
 
     private Laegemiddel laegemiddel;
-    // TODO Link til Laegemiddel
+
     // TODO constructor (med specifikation)
+
+    public Ordination(LocalDate startDen, LocalDate slutDen, Patient patient, Laegemiddel laegemiddel, double antal){
+        this.startDen = startDen;
+        this.slutDen = slutDen;
+        this.laegemiddel = laegemiddel;
+    }
 
     public LocalDate getStartDen() {
         return startDen;
