900736bedbb41c9d40fad3108352e6486acd3118
ellog <ellogovnor47@gmail.com>
2022-02-23 11:40:22 +0100

    DagligFast

--- Project/src/ordination/DagligFast.java
+++ Project/src/ordination/DagligFast.java
@@ -1,27 +1,45 @@
 package ordination;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
 
 public class DagligFast extends Ordination {
-
-
-    public DagligFast(LocalDate startDen, LocalDate slutDen, Patient patient, Laegemiddel laegemiddel) {
+    private final Dosis[]doses=new Dosis[4];
+    private double morgenAntal;
+    private double middagAntal;
+    private double aftenAntal;
+    private double natAntal;
+    public DagligFast(LocalDate startDen, LocalDate slutDen, Patient patient, Laegemiddel laegemiddel, double morgenAntal,double middagAntal,double aftenAntal,double natAntal) {
         super(startDen, slutDen, patient, laegemiddel);
+        doses[0]=new Dosis(LocalTime.of(8, 00), morgenAntal);
+        doses[1]=new Dosis(LocalTime.of(12, 00), middagAntal);
+        doses[2]=new Dosis(LocalTime.of(18, 00), aftenAntal);
+        doses[3]=new Dosis(LocalTime.of(24, 00), natAntal);
     }
-
     @Override
     public double samletDosis() {
-        return 0;
+        double sum=0;
+        for (Dosis dose : doses) {
+            sum += dose.getAntal();
+        }
+        return sum;
     }
 
     @Override
     public double doegnDosis() {
-        return 0;
+        double sum=0;
+        for (Dosis dose : doses) {
+            sum += dose.getAntal();
+        }
+        return sum/antalDage();
     }
 
     @Override
     public String getType() {
-        return null;
+        return getLaegemiddel().getNavn();
     }
-    // TODO
+
+
 }
