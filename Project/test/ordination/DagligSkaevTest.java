43c3cb1ab232b272026be58776aefd4f530cda84
ellog <ellogovnor47@gmail.com>
2022-02-25 10:22:43 +0100

    Master

--- Project/test/ordination/DagligSkaevTest.java
+++ Project/test/ordination/DagligSkaevTest.java
@@ -12,6 +12,8 @@ class DagligSkaevTest {
 
     @Test
     void samletDosis() {
+        //TC2
+        // kun 1 tidspunkt
         Laegemiddel laegemiddel=new Laegemiddel("Acetylsalicylsyre", 0.1, 0.15, 0.16, "Styk");
         Patient patient=new Patient("121256-0512", "Jane Jensen", 63.4);
         LocalTime[] kl = { LocalTime.of(12, 0)};
@@ -24,6 +26,8 @@ class DagligSkaevTest {
 
     @Test
     void doegnDosis() {
+        //TC2
+        //kun 1 tidspunkt
         Laegemiddel laegemiddel=new Laegemiddel("Acetylsalicylsyre", 0.1, 0.15, 0.16, "Styk");
         Patient patient=new Patient("121256-0512", "Jane Jensen", 63.4);
         LocalTime[] kl = { LocalTime.of(12, 0)};
@@ -32,5 +36,20 @@ class DagligSkaevTest {
         assertEquals(2, Controller.getController().opretDagligSkaevOrdination(LocalDate.of(2021, 1, 23),
                 LocalDate.of(2021, 1, 24), patient,
                 laegemiddel, kl, an).doegnDosis());
+
+
+        //TC4
+        // kl er 23:59 for at se om det virker da det er en ækvivalens punkt
+        LocalTime[] kl2 = { LocalTime.of(23, 59)};
+        assertEquals(2, Controller.getController().opretDagligSkaevOrdination(LocalDate.of(2021, 1, 23),
+                LocalDate.of(2021, 1, 24), patient,
+                laegemiddel, kl2, an).doegnDosis());
+
+        //TC11
+        // her er dosis kun 0.1
+        double[] an2 = {0.1};
+        assertEquals(0.1, Controller.getController().opretDagligSkaevOrdination(LocalDate.of(2021, 1, 23),
+                LocalDate.of(2021, 1, 24), patient,
+                laegemiddel, kl, an2).doegnDosis());
     }
     }
